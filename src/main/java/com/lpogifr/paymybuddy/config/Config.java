76f003f6e78458757b0c7bf490c5c53d56530a81
Guillaume Pot <guillaume.pot@hotmail.com>
2023-12-23 17:39:45 +0100

    First commit

---
+++ src/main/java/com/lpogifr/paymybuddy/config/Config.java
@@ -0,0 +1,12 @@
+package com.lpogifr.paymybuddy.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+  //@Bean
+  //CommandLineRunner initDatabase(){return args -> }
+
+}
